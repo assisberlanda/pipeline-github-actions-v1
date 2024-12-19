2b2bff6fafeaf89dc6289398d4d76a47e0db73f8
Assis Berlanda de Medeiros <berlanda.medeiros@gmail.com>
2024-12-19 11:44:17 +0100

    Adicionando arquivos de configuração

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "danielgilterraformstate"
+    container_name       = "remote-state"
+    key                  = "pipeline-azure-devops/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "assisberlanda"
+      managed-by = "terraform"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "assisberlanda-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "assisberlandaterrastate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
